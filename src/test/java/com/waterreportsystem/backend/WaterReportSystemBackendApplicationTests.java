a6e58461c6d23a5b0010db8900bd3097e3e87bd2
Kgalalelo Mokotedi <mokotedikgalalelo93@gmail.com>
2026-07-16 08:14:19 +0200

    Initial Spring Boot project setup

---
+++ src/test/java/com/waterreportsystem/backend/WaterReportSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waterreportsystem.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterReportSystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
