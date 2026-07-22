2a26d30c644aec3b03e6d93c0b49fc760d4bf58d
Alonzo Joshua Diale <111920385+AJ-Diale@users.noreply.github.com>
2026-07-22 09:21:15 +0200

    Database amended

--- src/main/java/com/waterreportsystem/backend/ModuleApplication.java
+++ src/main/java/com/waterreportsystem/backend/ModuleApplication.java
@@ -1,4 +1,4 @@
-package com.waterreport;
+package com.waterreportsystem.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
